749bc5b37e0270f7ef772113913ebe79ac4edff2
Abdullah Munshi <stackbind@gmail.com>
2025-09-08 09:58:10 +0600

    fix: remove catagorized render in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import HomeSkeleton from "../components/skeleton/HomeSkeleton";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const Home = () => {
-  const [items, setItems] = useState([]);
+  const [plants, setPlants] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchPlants = async () => {
@@ -22,25 +22,7 @@ const Home = () => {
           throw new Error("Failed to fetch plants");
         }
         const data = await res.json();
-
-        const groups = {};
-        data.data.forEach((plant) => {
-          const catTitle = plant.category?.title || "Uncategorized";
-          if (!groups[catTitle]) {
-            groups[catTitle] = [];
-          }
-          groups[catTitle].push(plant);
-        });
-        const groupedPlants = Object.entries(groups).map(
-          ([category, plants]) => ({
-            category,
-            plants,
-          })
-        );
-
-        console.log("groups", groupedPlants);
-
-        setItems(groupedPlants);
+        setPlants(data.data);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching plants:", error);
@@ -54,27 +36,21 @@ const Home = () => {
       {isLoading ? (
         <HomeSkeleton />
       ) : (
-        items.length > 0 &&
-        items.map((item) => (
-          <div className="mb-10" key={item.category}>
-            <h2 className="text-2xl font-semibold mb-4">{item.category}</h2>
-            <div className="grid grid-cols-4 gap-4">
-              {item.plants.length > 0 &&
-                item.plants.map((plant) => (
-                  <ProductCard
-                    key={plant.id}
-                    name={plant.title}
-                    subtitle={plant.subtitle}
-                    image={
-                      plant.thumbnail.formats?.small.url || plant.thumbnail.url
-                    }
-                    description={plant.description}
-                    cost={plant.price}
-                  />
-                ))}
-            </div>
-          </div>
-        ))
+        <div className="grid grid-cols-4 gap-4">
+          {plants.length > 0 &&
+            plants.map((plant) => (
+              <ProductCard
+                key={plant.id}
+                name={plant.title}
+                subtitle={plant.subtitle}
+                image={
+                  plant.thumbnail.formats?.small.url || plant.thumbnail.url
+                }
+                description={plant.description}
+                cost={plant.price}
+              />
+            ))}
+        </div>
       )}
     </Container>
   );
