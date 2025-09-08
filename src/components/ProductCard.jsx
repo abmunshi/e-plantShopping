31156159660018ff56791c11601a4e8ebb1bfa4a
Abdullah Munshi <stackbind@gmail.com>
2025-09-08 14:29:01 +0600

    ui:Improve product card design

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@heroui/react";
 import { useDispatch } from "react-redux";
 import { addItem } from "../redux/slices/CartSlice";
+import { StarIcon } from "@heroicons/react/16/solid";
 const ProductCard = ({ name, image, description, cost }) => {
   const dispatch = useDispatch();
   const handleAddToCart = () => {
@@ -15,10 +16,35 @@ const ProductCard = ({ name, image, description, cost }) => {
           alt="thumbnail"
         />
       </div>
-      <h4 className="font-semibold">{name}</h4>
-      <p className="text-[15px] leading-tight mb-2">{description}</p>
-      <p className="mb-2">{cost}</p>
-      <Button onPress={handleAddToCart}>Add To Cart</Button>
+      <h4 className="font-bold text-base mb-1">{name}</h4>
+      <p className="text-sm leading-tight mb-3 line-clamp-2">{description}</p>
+      <div className="flex items-center gap-2 justify-between mb-3.5">
+        <p className="flex items-center gap-1">
+          <span className="inline-block font-bold text-base">${cost - 5}</span>
+          <span className="line-through inline-block font-normal text-xs ml-1 text-black/60 leading-tight">
+            ${cost}
+          </span>
+        </p>
+        <p className="flex items-center gap-1">
+          <span className="inline-flex items-center gap-[2px]">
+            <StarIcon className="w-4 h-4 text-yellow-400" />
+            <strong className="inline-block text-sm font-bold">4.5</strong>
+          </span>
+          <span className="inline-block text-black/60 text-xs">
+            (49 Reviews)
+          </span>
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4">
+        <Button className="rounded-none">View Details</Button>
+        <Button
+          onPress={handleAddToCart}
+          className="bg-primary rounded-none text-white"
+        >
+          Add To Cart
+        </Button>
+      </div>
     </div>
   );
 };
